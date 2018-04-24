dc986232e3c27e785a94f893fcaa9fe8cf266bc9
ghoiufyia <ghoiufyia@163.com>
2018-04-24 14:36:32 +0800

    src

---
+++ src/YuntongxunServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Shangning\Yuntongxun;
+
+use Illuminate\Support\ServiceProvider;
+use Shangning\Yuntongxun\Yuntongxun;
+
+class YuntongxunServiceProvider extends ServiceProvider
+{
+    /**
+     * 服务提供者加是否延迟加载.
+     *
+     * @var bool
+     */
+    protected $defer = true; // 延迟加载服务
+
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        $this->publishes([__DIR__ . '/../config/yuntongxun.php' =>config_path('yuntongxun.php')]);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton('yuntongxun', function ($app) {
+            return new Yuntongxun($app['config']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['yuntongxun'];
+    }
+}
